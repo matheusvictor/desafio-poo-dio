2a31cce65f7e95aed763a8a92d16d2b78ee21d90
Matheus Victor <matheusvictor.salles@gmail.com>
2022-04-01 09:07:57 -0300

    Cria instâncias das Classes
    As classes criadas foram instanciadas para demostrar a funcionalidade da regra de negócio implementada através da utilização ou não dos métodos criados

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.Bootcamp;
 import br.com.dio.desafio.Curso;
+import br.com.dio.desafio.Dev;
 import br.com.dio.desafio.Mentoria;
 
 import java.time.LocalDate;
@@ -21,6 +23,94 @@ public class Main {
         mentoria.setDescricao("Conteúdos gratuitos!");
         mentoria.setData(LocalDate.now());
 
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Aprenda Java na prática");
+        bootcamp.getConteudos().add(primeiroCurso);
+        bootcamp.getConteudos().add(segundoCurso);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev participante_um = new Dev();
+        participante_um.setNome("Matheus");
+        participante_um.inscreverBootcamp(bootcamp);
+
+        System.out.println("------------------------------------------------");
+
+        System.out.println
+                ("Conteúdos em que "
+                        + participante_um.getNome() + " está inscrito(a): "
+                        + participante_um.getConteudosInscritos()
+                );
+        System.out.println("Quantidade de cursos a fazer: " + participante_um.getConteudosInscritos().size());
+
+        System.out.println("------------------------------------------------");
+
+        System.out.println
+                ("Conteúdos concluídos por "
+                        + participante_um.getNome() + ": "
+                        + participante_um.getConteudosConcluidos()
+                );
+        System.out.println("Quantidade de cursos concluídos: " + participante_um.getConteudosConcluidos().size());
+        System.out.println
+                ("Total de XP de "
+                        + participante_um.getNome() + ": "
+                        + participante_um.calcularTotalXp()
+                );
+
+        System.out.println("------------------------------------------------");
+
+        participante_um.progredir();
+        participante_um.progredir();
+        System.out.println("Depois de concluir alguns cursos...");
+
+        System.out.println("Quantidade de cursos a fazer: " + participante_um.getConteudosInscritos().size());
+        System.out.println("Quantidade de cursos concluídos: " + participante_um.getConteudosConcluidos().size());
+        System.out.println
+                ("Total de XP de "
+                        + participante_um.getNome() + ": "
+                        + participante_um.calcularTotalXp()
+                );
+
+        System.out.println("------------------------------------------------");
+
+        Dev participante_dois = new Dev();
+        participante_dois.setNome("Victor");
+        participante_dois.inscreverBootcamp(bootcamp);
+
+        System.out.println
+                ("Conteúdos em que "
+                        + participante_dois.getNome() + " está inscrito(a): "
+                        + participante_dois.getConteudosInscritos()
+                );
+        System.out.println("Quantidade de cursos a fazer: " + participante_dois.getConteudosInscritos().size());
+
+        System.out.println("------------------------------------------------");
+
+        System.out.println
+                ("Conteúdos concluídos por "
+                        + participante_dois.getNome() + ": "
+                        + participante_dois.getConteudosConcluidos()
+                );
+        System.out.println("Quantidade de cursos concluídos: " + participante_dois.getConteudosConcluidos().size());
+        System.out.println
+                ("Total de XP de "
+                        + participante_dois.getNome() + ": "
+                        + participante_dois.calcularTotalXp()
+                );
+
+        System.out.println("------------------------------------------------");
+
+        System.out.println("Como cursos não foram concluídos...");
+
+        System.out.println("Quantidade de cursos a fazer: " + participante_dois.getConteudosInscritos().size());
+        System.out.println("Quantidade de cursos concluídos: " + participante_dois.getConteudosConcluidos().size());
+        System.out.println
+                ("Total de XP de "
+                        + participante_dois.getNome() + ": "
+                        + participante_dois.calcularTotalXp()
+                );
+
+        System.out.println("------------------------------------------------");
 
     }
 }
